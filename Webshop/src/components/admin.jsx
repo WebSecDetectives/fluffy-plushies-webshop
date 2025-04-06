4386e8844791a2b9ce89a848cc63bc41d848d8a0
Martin-Niemann <95416410+Martin-Niemann@users.noreply.github.com>
2025-04-06 14:30:04 +0200

    addded navbar and routes

---
+++ Webshop/src/components/admin.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Admin() {
+
+    return (
+        <div>
+            Hello this is the admin page
+        </div>
+    )
+}
+
+export default Admin
---
+++ Webshop/src/components/products.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Products() {
+
+    return (
+        <div>
+            Hello this is the products page
+        </div>
+    )
+}
+
+export default Products
