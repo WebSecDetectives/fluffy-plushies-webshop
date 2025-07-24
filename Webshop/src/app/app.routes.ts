d9bda6818f13031e729259548e67fbba577c269e
SirMeows <araelbrighteyes@gmail.com>
2025-07-24 15:41:24 +0200

    move frontend to mono-repo

---
+++ Webshop/src/app/app.routes.ts
@@ -0,0 +1,19 @@
+import { Routes } from '@angular/router';
+import { CreateAccount } from './create-account/create-account';
+import { UpdateAccount } from './update-account/update-account';
+import { Home } from './home/home';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home
+  },
+  {
+    path: 'create',
+    component: CreateAccount
+  },
+  {
+    path: 'update',
+    component: UpdateAccount
+  }
+];
---
+++ Webshop/src/enviroments/enviroment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  baseUrlIdentity: 'http://localhost:8081/identity',
+  baseUrlInventory: 'http://localhost:8082/inventory',
+};
